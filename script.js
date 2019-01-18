1e31a8416ff7212e559277330a3e538ff59d8935
Alex Coe <alex.r.coe@gmail.com>
2019-01-18 13:46:41 -0600

    added header icons

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let dropdown = document.getElementById('drop-down');
+let nav1 = document.getElementById('nav1');
+let nav2 = document.getElementById('nav2');
+let nav3 = document.getElementById('nav3');
+
 dropdown.addEventListener('click', function() {
-    let nav1 = document.getElementById('nav1');
-    let nav2 = document.getElementById('nav2');
-    let nav3 = document.getElementById('nav3');
-    
-    nav1.innerHTML = "<a href="portfolio.html">Portfolio</a>";
-    nav2.innerHTML = "<a href="about.html">About</a>";
-    nav3.innerHTML = "<a href="contact.html">Contact</a>";
+    return nav1.innerHTML= "<a href="portfolio.html">Portfolio</a>";
+    return nav2.innerHTML = "<a href="about.html">About</a>";
+    return nav3.innerHTML = "<a href="contact.html">Contact</a>";
 })
